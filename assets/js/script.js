fb1a74578f7c2322f3b8b63edb64dad54c77f6ae
Mjumel <m.jumel@hotmail.fr>
2022-01-28 15:41:50 +0100

    Modifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,13 @@ btnMenu.addEventListener('click', function () {
 
 })
 
+var info = document.querySelector(".info")
 
-info.addEventListener('scroll', function () {
+window.addEventListener('scroll', function () {
     
-    position = window.scrollY;
-    if (position >= 0) {
+  
+    if (window.scrollY >= 0) {
+       
         info.style.opacity = 1;
         info.style.transition = '2s';
 
@@ -20,3 +22,31 @@ info.addEventListener('scroll', function () {
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
+
+
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  var items = document.querySelectorAll(".timeline li");
+
+// code for the isElementInViewport function
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add("in-view");
+    }
+  }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+
